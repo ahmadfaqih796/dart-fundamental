dfad21035e691ce5c0475644389c9d3c12cb61c0
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-01-13 17:27:49 +0700

    variabel

---
+++ bin/variabel.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = "Ahmad Faqih Arifin";
+  var firstName = "Ahmad";
+  // final ini nilai mutlak tidak bisa di deklerasikan ulang
+  final lastName = "Faqih Arifin";
+  print(name);
+  print(firstName);
+  firstName = "Aji";
+  print(firstName);
+  print(lastName);
+}
