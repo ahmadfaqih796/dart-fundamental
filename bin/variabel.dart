96ef41bfaabbf2f116518c200eeb580a57899d2c
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-01-13 17:32:52 +0700

    final dan const

--- bin/variabel.dart
+++ bin/variabel.dart
@@ -8,4 +8,19 @@ void main() {
   firstName = "Aji";
   print(firstName);
   print(lastName);
+
+  final array1 = [1, 2, 3];
+  const array2 = [1, 2, 3];
+
+  // isi bisa di ubah
+  array1[0] = 10;
+  // tidak bisa di dekrelasikan
+  // array1 = [0,0,0];
+
+  // tidak bisa di ubah
+  // array2 = [0,0,0];
+  // array2[0] = 10;
+
+  print(array1);
+  print(array2);
 }
